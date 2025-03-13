00c61e72c0063a41dc19739540f5aa93f619fd4b
Celso <celsortc@outlook.com>
2025-03-12 21:54:39 -0300

    estudos

---
+++ ExemplosAulas/TiposDeDados/script.js
@@ -0,0 +1,16 @@
+var nome = "Celso";
+var sobrenome = "Toledo";
+console.log(nome);
+
+var frase = nome + " tem 24 anos";
+console.log(frase);
+
+var idade = 24;
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto + " tem " + idade + " anos");
+
+var frase2 = "It's time";
+var fraseCompleta = `${frase2} to DUDUDUDU`;
+console.log(fraseCompleta);
+
+console.log(typeof nomeCompleto);
